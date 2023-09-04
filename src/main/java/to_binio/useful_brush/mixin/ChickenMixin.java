147f79ae37c91717188baf3078a680edff2c3622
tobinio <Tobias.frischmann1@gmail.com>
2023-09-04 10:47:49 +0200

    brush chicken

---
+++ src/main/java/to_binio/useful_brush/mixin/ChickenMixin.java
@@ -0,0 +1,19 @@
+package to_binio.useful_brush.mixin;
+
+import net.minecraft.entity.passive.ChickenEntity;
+import net.minecraft.entity.passive.SheepEntity;
+import net.minecraft.item.Items;
+import org.spongepowered.asm.mixin.Mixin;
+import to_binio.useful_brush.BrushAble;
+
+@Mixin (ChickenEntity.class)
+public class ChickenMixin implements BrushAble {
+    @Override
+    public boolean brush() {
+        ChickenEntity chicken = (ChickenEntity) (Object) this;
+
+        chicken.dropItem(Items.FEATHER.asItem(), 1);
+
+        return true;
+    }
+}
